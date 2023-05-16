8beafb44c0adff7cf4271f5d3b34fe604aa5b33d
Jae <giffordjacob0@gmail.com>
2023-05-16 16:31:05 +0800

    init router and query client

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename='/'>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
