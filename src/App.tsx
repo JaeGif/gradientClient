6bd204dba5767fc43afd06c1080460cc6054209a
Jae <giffordjacob0@gmail.com>
2023-05-23 08:22:22 +0800

    auth provider started

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
 import React, { useContext } from 'react';
+const ThemeContext = React.createContext<'light' | 'dark'>('dark');
 
 function App() {
-  const ThemeContext = React.createContext<'light' | 'dark'>('dark');
-  return <></>;
+  return (
+    /*  <Routes>
+      <Route path='/' element={}>
+    </Routes> */
+    <>stuff</>
+  );
 }
 
 export default App;
--- src/utils/AuthProvider.tsx
+++ src/utils/AuthProvider.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState, createContext, ReactNode } from 'react';
+import { User } from '../types/Interfaces';
+const AuthContext = createContext<{
+  user: User | null;
+  login: (user: User) => void;
+  logout: () => void;
+} | null>(null);
 
-function AuthProvider() {
-  return <div>AuthProvider</div>;
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = (user: User) => {
+    setUser(user);
+  };
+  const logout = () => {
+    setUser(null);
+  };
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
-
-export default AuthProvider;
