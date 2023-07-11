9962e2d49c11da9fdd7b304cfd68f5bcb22753f1
Jae <giffordjacob0@gmail.com>
2023-07-11 09:54:49 +0800

    standards only display on exercises with standards

--- src/components/analytics/ChartAnalysis.tsx
+++ src/components/analytics/ChartAnalysis.tsx
@@ -1,16 +1,19 @@
 import RegressionAnalysis from './RegressionAnalysis';
 import StandardsAnalysis from './StandardsAnalysis';
-
+import { exerciseInStandards } from '../../utils/fnSheet/utilities';
 type ChartAnalysisProps = {
   exerciseId: string;
   showAbsolute: boolean;
 };
+const userGender = 'm';
 
 function ChartAnalysis({ exerciseId, showAbsolute }: ChartAnalysisProps) {
   return (
     <div className='min-w-fit'>
       <RegressionAnalysis exerciseId={exerciseId} showAbsolute={showAbsolute} />
-      <StandardsAnalysis exerciseId={exerciseId} average={!showAbsolute} />
+      {exerciseInStandards(exerciseId, userGender) && (
+        <StandardsAnalysis exerciseId={exerciseId} average={!showAbsolute} />
+      )}
     </div>
   );
 }
