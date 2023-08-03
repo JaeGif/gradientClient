6eb8aaac56e26602a0da12ec2955d3811f1a1dae
Jae <giffordjacob0@gmail.com>
2023-08-03 11:14:30 +0800

    stats on dashboard editing working and optimistic updates

--- src/components/dashboard/statistics/UserBodyFat.tsx
+++ src/components/dashboard/statistics/UserBodyFat.tsx
@@ -1,18 +1,59 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useAuth } from '../../../utils/AuthProvider';
 import { GoalContext } from '../../../pages/home/Dashboard';
 import { UserQueryResult } from '../../../types/Interfaces';
+import useUserQuery from '../../../hooks/useUserQuery';
+import { useQueryClient } from '@tanstack/react-query';
 
 type UserBodyFatProps = {
   user: UserQueryResult;
 };
 function UserBodyFat({ user }: UserBodyFatProps) {
-  const userBodyFat = user.bodyFatPercentage;
+  const userBodyFat = user.bodyFatPercentage!;
   const bodyFatGoal = useContext(GoalContext)?.bodyFatPercentage;
+  const [editing, setEditing] = useState(false);
+  const [newBodyFatPercentage, setNewBodyFatPercentage] =
+    useState<number>(userBodyFat);
+  const putUserStats = useUserQuery().putUserStatsMutation();
+  const queryClient = useQueryClient();
+  const submitEdits = () => {
+    setEditing(false);
+
+    if (newBodyFatPercentage !== userBodyFat) {
+      putUserStats.mutate(
+        { bodyFatPercentage: newBodyFatPercentage },
+        {
+          onSuccess: () => {
+            queryClient.invalidateQueries({
+              queryKey: ['user', { id: user.id }],
+            });
+          },
+        }
+      );
+    }
+  };
   return (
     <div className='relative shadow-md p-2 rounded-lg h-full flex w-1/4 flex-col justify-center items-center gap-2 text-center'>
       <h2 className='text-xl'>Body Fat Percentage</h2>
-      <p className='text-4xl text-blue-500'>{userBodyFat}%</p>
+      {editing ? (
+        <input
+          type='number'
+          className='p-2'
+          autoFocus
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') submitEdits();
+          }}
+          onBlur={() => {
+            submitEdits();
+          }}
+          placeholder={`${userBodyFat}`}
+          onChange={(e) => {
+            setNewBodyFatPercentage(parseFloat(e.target.value));
+          }}
+        />
+      ) : (
+        <p className='text-4xl text-blue-500'>{newBodyFatPercentage}%</p>
+      )}
       {bodyFatGoal && <p className=''>Goal: {bodyFatGoal}%</p>}
       <span className='absolute bottom-0 flex justify-end p-3 w-full'>
         <img
@@ -21,7 +62,9 @@ function UserBodyFat({ user }: UserBodyFatProps) {
           className='h-6 hover:cursor-pointer hover:animate-bounce'
           aria-label='record body fat'
           src='/favicons/edit.svg'
-          onClick={() => {}}
+          onClick={() => {
+            setEditing(true);
+          }}
         />
       </span>
     </div>
--- src/components/dashboard/statistics/UserWeight.tsx
+++ src/components/dashboard/statistics/UserWeight.tsx
@@ -1,20 +1,62 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useAuth } from '../../../utils/AuthProvider';
 import { GoalContext } from '../../../pages/home/Dashboard';
 import { UserQueryResult } from '../../../types/Interfaces';
-
+import { useQueryClient } from '@tanstack/react-query';
+import useUserQuery from '../../../hooks/useUserQuery';
 type UserWeightProps = {
   user: UserQueryResult;
 };
 function UserWeight({ user }: UserWeightProps) {
   const userWeight = user.weight;
   const weightGoal = useContext(GoalContext)?.weight;
+  const [editing, setEditing] = useState(false);
+  const [newWeight, setNewWeight] = useState(userWeight);
+
+  const putUserStats = useUserQuery().putUserStatsMutation();
+  const queryClient = useQueryClient();
+  const submitEdits = () => {
+    setEditing(false);
+
+    if (newWeight !== userWeight) {
+      putUserStats.mutate(
+        { weight: newWeight },
+        {
+          onSuccess: () => {
+            queryClient.invalidateQueries({
+              queryKey: ['user', { id: user.id }],
+            });
+          },
+        }
+      );
+    }
+  };
   return (
     <div className='relative shadow-md p-2 rounded-lg h-full flex w-1/4 flex-col justify-center items-center gap-2 text-center'>
       <h2 className='text-xl'>Current Weight</h2>
-      <p className='text-4xl text-blue-500'>
-        {userWeight} {user.preferences.unit}
-      </p>
+
+      {editing ? (
+        <input
+          type='number'
+          className='p-2'
+          autoFocus
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') submitEdits();
+          }}
+          onBlur={() => {
+            submitEdits();
+          }}
+          placeholder={`${userWeight}`}
+          onChange={(e) => {
+            setNewWeight(parseFloat(e.target.value));
+          }}
+        />
+      ) : (
+        <p className='text-4xl text-blue-500'>
+          {newWeight} {user.preferences.unit}
+        </p>
+      )}
+
       {weightGoal && (
         <p>
           Goal: {weightGoal} {user.preferences.unit}
@@ -27,7 +69,9 @@ function UserWeight({ user }: UserWeightProps) {
           alt='record'
           aria-label='record weight'
           src='/favicons/edit.svg'
-          onClick={() => {}}
+          onClick={() => {
+            setEditing(true);
+          }}
         />
       </span>
     </div>
