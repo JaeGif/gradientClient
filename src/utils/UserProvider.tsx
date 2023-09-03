1bbe9b55aaa19ba1756a0a0571d19db02b67d18a
Jae <giffordjacob0@gmail.com>
2023-09-03 13:12:27 +0800

    data loading and error messages added for user find

--- src/utils/UserProvider.tsx
+++ src/utils/UserProvider.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext, ReactNode, useEffect } from 'react';
 import { UserQueryResult } from '../types/Interfaces';
 import useUserQuery from '../hooks/useUserQuery';
+import TailSpin from 'react-loading-icons/dist/esm/components/tail-spin';
 
 const UserContext = React.createContext<UserQueryResult | undefined>(undefined);
 
@@ -9,13 +10,27 @@ export function UserProvider({ children }: { children: ReactNode }) {
   const [freshUserData, setFreshUserData] = useState<
     UserQueryResult | undefined
   >(undefined);
-
   useEffect(() => {
     if (userQuery.data) {
       setFreshUserData(userQuery.data);
     }
+    console.log(userQuery.status);
   }, [userQuery.data, userQuery.status]);
-
+  if (userQuery.status === 'loading') {
+    return (
+      <div className='w-screen h-screen flex flex-col justify-center items-center gap-5'>
+        <h2>Loading Data</h2>
+        <TailSpin stroke='#333333' />
+      </div>
+    );
+  }
+  if (userQuery.status === 'error') {
+    return (
+      <div className='flex justify-center items-center h-screen w-screen'>
+        There seems to be a network issue <br /> Please check your connection.
+      </div>
+    );
+  }
   return (
     <UserContext.Provider value={freshUserData}>
       {freshUserData && children}
